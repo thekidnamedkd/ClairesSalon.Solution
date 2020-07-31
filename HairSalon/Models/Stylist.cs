9bd740a29269837161d1190f1be469f8c45d3cef
Kevin Davis <thekidnamedkd@gmail.com>
2020-07-31 10:35:14 -0700

    add properties for Stylist model.

--- HairSalon/Models/Stylist.cs
+++ HairSalon/Models/Stylist.cs
@@ -3,7 +3,14 @@ using System.Collections.Generic;
 namespace HairSalon.Models
 {
   public class Stylist
-  {
-    
-  }
+    {
+        public Stylist()
+        {
+            this.Client = new HashSet<Client>();
+        }
+
+        public int StylistId { get; set; }
+        public string Name { get; set; }
+        public virtual ICollection<Client> Clients { get; set; }
+    }
 }
