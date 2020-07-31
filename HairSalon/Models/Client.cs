d417b12da8904a5729d7b9e8eae90294a46e151f
Kevin Davis <thekidnamedkd@gmail.com>
2020-07-31 10:26:19 -0700

    add properties to Client class and set properties for the database context to interface between classes and tables.

--- HairSalon/Models/Client.cs
+++ HairSalon/Models/Client.cs
@@ -2,6 +2,11 @@ namespace HairSalon.Models
 {
   public class Client
   {
-    
+    public int ClientId { get; set; }
+    public int Name { get; set; }
+    public string Style { get; set; }
+    public int Phone { get; set; }
+    public string StylistId { get; set; }
+    public virtual Stylist Stylist { get; set; }
   }
 }
