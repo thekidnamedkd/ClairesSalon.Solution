88efb663c6e2c4be13995c72d6bc78e63c0ee488
Kevin Davis <thekidnamedkd@gmail.com>
2020-08-15 16:22:16 -0700

    delete old schema SQL containing data and resubmit with empty new file.

--- kevin_davis.sql
+++ kevin_davis.sql
@@ -24,23 +24,13 @@ DROP TABLE IF EXISTS `Clients`;
  SET character_set_client = utf8mb4 ;
 CREATE TABLE `Clients` (
   `ClientId` int(11) NOT NULL AUTO_INCREMENT,
-  `ClientName` varchar(255) NOT NULL,
-  `Style` varchar(255) NOT NULL,
-  `Phone` varchar(255) NOT NULL,
-  `StylistId` int(11) NOT NULL DEFAULT '0',
+  `ClientName` varchar(255) DEFAULT NULL,
+  `Style` varchar(255) DEFAULT NULL,
+  `Phone` varchar(255) DEFAULT NULL,
+  `StylistId` int(11) NOT NULL,
   PRIMARY KEY (`ClientId`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `Clients`
---
-
-LOCK TABLES `Clients` WRITE;
-/*!40000 ALTER TABLE `Clients` DISABLE KEYS */;
-INSERT INTO `Clients` VALUES (1,'Kevin','Short','9715707426',2),(2,'Rachel','Short','9715707426',3),(3,'Mike','Long','50333044003',1);
-/*!40000 ALTER TABLE `Clients` ENABLE KEYS */;
-UNLOCK TABLES;
 
 --
 -- Table structure for table `Stylists`
@@ -53,18 +43,8 @@ CREATE TABLE `Stylists` (
   `StylistId` int(11) NOT NULL AUTO_INCREMENT,
   `StylistName` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`StylistId`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `Stylists`
---
-
-LOCK TABLES `Stylists` WRITE;
-/*!40000 ALTER TABLE `Stylists` DISABLE KEYS */;
-INSERT INTO `Stylists` VALUES (1,'Richie'),(2,'Sarah'),(3,'Von');
-/*!40000 ALTER TABLE `Stylists` ENABLE KEYS */;
-UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -75,4 +55,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-08-13  1:28:19
+-- Dump completed on 2020-08-15 15:49:10
